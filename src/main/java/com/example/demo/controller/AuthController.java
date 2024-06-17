ac44ef31002128d1e5a429ad198bcdda464ae679
estudiante <estudiante@172.18.22.4>
2024-06-17 11:50:12 -0500

    Finalización de generación de token de seguridad

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,20 +1,50 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.util.JwtUtil;
 
 @RestController
 public class AuthController {
 	
-	@GetMapping("/authenticate")
+	@Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+	
+	@GetMapping("/status")
 	public String auth() {
 		return "OK";
 	}
 	
 	@PostMapping("/authenticate")
-	public String crearAuth() {
-		return "OK";
-	}
+	public String createAuthenticationToken(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+            );
+        } catch (AuthenticationException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return jwt;
+    }
 
 }
---
+++ src/main/java/com/example/demo/util/JwtUtil.java
@@ -0,0 +1,37 @@
+package com.example.demo.util;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtUtil {
+	
+	private String secretKeyString = "9a4f2c8d3b7a1e6f45c8a0b3f267d8b1d4e6f3c8a9d2b5f8e3a9c8b5f6v8a3d9";
+	
+	public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+    	
+    	SecretKey secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes());
+        return Jwts.builder()
+        		.subject(subject)
+        		.issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .claims(claims)
+                .signWith(secretKey)
+                .compact();
+    }
+
+}
